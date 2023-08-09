a68c53a14943b1490c869bc7feca27597129c858
Fernanda-Cidade <fernanda.cidade@outlook.com>
2023-08-08 21:27:27 -0300

    adição da pagina Serviços

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,10 @@ import ReactDOM from 'react-dom/client'
 //import App from './App.tsx'
 import './index.css'
 import Home from './pages/Home'
+import Servicos from './pages/Servicos/index'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Home />
+    <Servicos />
   </React.StrictMode>,
 )
