9f1f96a34072be1c23f97781d6a8ccef2090f5ed
Fernanda-Cidade <fernanda.cidade@outlook.com>
2023-08-10 19:43:23 -0300

    Add de componentes

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,30 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Route, Routes, BrowserRouter } from "react-router-dom"
+
 //import App from './App.tsx'
 import './index.css'
+import Header from './Componentes/Header'
 import Home from './pages/Home'
-import Servicos from './pages/Servicos/index'
+import Footer from './Componentes/Footer'
+// import Servicos from './pages/Servicos/index'
+
+import CadastroDev from './pages/CadastroDev'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Servicos />
-  </React.StrictMode>,
+    <BrowserRouter>
+      <Header/>
+      <Routes>
+        <Route>
+
+        </Route>
+
+      </Routes>
+      <Footer/>  {/* comentar o footer para testar o CadastroDev! */}
+      <Home/>
+      <CadastroDev/>
+      
+    </BrowserRouter>
+   </React.StrictMode>,
 )
